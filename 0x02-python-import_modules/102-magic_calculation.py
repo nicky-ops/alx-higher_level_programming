b0a4dfc8655399100dfb4992990384a65f64efa8
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-08-22 10:08:55 -0700

    Revert "Python bytecode"
    This reverts commit de42473c2758bc6086c9680ba1740aea62d6488a.
    
    Python bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,9 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    def add(x, y):
-        return x + y
-
-    def sub(x, y):
-        return x - y
-
+    add, sub = magic_calculation_102.add, magic_calculation_102.sub
     if a < b:
         c = add(a, b)
         for i in range(4, 7):
             c = add(c, i)
         return c
     return sub(a, b)
-
