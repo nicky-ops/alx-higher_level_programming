f0911a175e69d99c722eb99b5337566498033fae
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-08-28 11:33:34 -0700

    print x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(int(x)):
-        try:
-            print("{}".format(my_list[i]), end="")
-        except TypeError:
-            print('Enter a valid integer')
+    try:
+        count = 0
+        for element in my_list:
+            if count < x:
+                print(element, end='')
+                count += 1
+        print()
+        return count
+    except Exception as e:
+        print("An error occured:", e)
+        return count
