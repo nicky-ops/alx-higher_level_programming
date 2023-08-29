5bdc852c370f1480fd255e9a55cc2233ba1cd288
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-08-29 12:15:14 -0700

    public instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defines class square"""
+
+
+class Square:
+    """square class with private instance attribute"""
+    def __init__(self, size=0):
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
+
+    def area(self):
+        return self.__size ** 2
