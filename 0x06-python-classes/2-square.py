8ceec761a2f328373d4091d6af4373be4e5f9e95
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-08-29 12:09:09 -0700

    square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,11 @@
 
 
 class Square:
-    """empty square"""
     def __init__(self, size=0):
         if type(size) != int:
-            raise TypeError("size must be an Integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
         else:
-            self.__size = size
+            raise TypeError('size must be an integer')
