b7ccb454a41bc1bb900c977b05c8efdee0bfe1b1
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-09-14 03:02:58 -0700

    fixed documentation error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,9 @@ function to add two integers
 
 
 def add_integer(a, b=98):
+    """
+    function takes two integer/floats and returns the sum of the two
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
