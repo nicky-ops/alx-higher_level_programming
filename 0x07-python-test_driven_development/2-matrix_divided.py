bc06c00dbc2073e72f91ab546643f5be4d6c998d
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-09-14 09:19:48 -0700

    matrix division

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -24,6 +24,6 @@ def matrix_divided(matrix, div):
     for i in matrix:
         for j in i:
             result =float("{:.2f}".format(j/div))
-        lst.append(result)
+            lst.append(result)
     new_matrix.append(lst)
     return new_matrix
