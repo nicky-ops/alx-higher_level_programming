959cba72ec3c8ad6cd7a8db7de02245bb14b8640
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-09-14 07:22:52 -0700

    divide all elements of a matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Matrix division
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    function that takes a matrix and an integer(div) as its arguments and divides each element of the matrix by integer(div)
+    returns a new matrix
+    """
+    new_matrix = []
+    lst = []
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    for i in matrix:
+        for j in i:
+            if type(j) not in [int, float]:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for i in range(0, (len(matrix) - 1)):
+        if len(matrix[i]) != len(matrix[i + 1]):
+            raise TypeError("Each row of the matrix must have the same size")
+    for i in matrix:
+        for j in i:
+            result =float("{:.2f}".format(j/div))
+            lst.append(result)
+        new_matrix.append(lst)
+    return new_matrix
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(matrix_divided(matrix, 3))
+print(matrix)
