9c31fd72d9c2d858e72482939ec73264f316aeb9
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-09-15 01:04:29 -0700

    fixed documentation error

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,6 +5,10 @@ Function to print full names
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    takes two arguments first_name an d last_name
+    prints first name and last name
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
