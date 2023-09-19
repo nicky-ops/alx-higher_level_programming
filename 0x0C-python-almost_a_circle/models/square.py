fca2d2c24fee5885cbe2316851abc0b6e3dbe347
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-09-19 06:34:23 -0700

    square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+"""
+Defines a Square class
+"""
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits attributes from Rectangle class
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        class constructor - initializes a new square
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        String representation of the class Square
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
