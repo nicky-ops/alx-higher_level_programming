b36ddbb8149ecfbf506d05d4b4ce871f8dc7f990
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-09-19 02:38:26 -0700

    Python package with base class module

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Base class for all other classes in the project
+"""
+
+
+class Base:
+    """
+    Base class contains private class attribute nd_objects.
+    Class constructor
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
