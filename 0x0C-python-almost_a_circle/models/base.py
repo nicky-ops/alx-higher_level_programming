ad9f7c54152e7bd83cd6f16ece8b1496598f11b6
Nickson Kipruto Rop <nickyrutto@gmail.com>
2023-09-19 07:17:11 -0700

    json string representation of a dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Base class for all other classes in the project
 """
+import json
 
 
 class Base:
@@ -12,8 +13,15 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ initializes Base class"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == {}:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
