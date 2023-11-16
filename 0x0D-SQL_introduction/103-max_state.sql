a762c5e598584fb4ea0176c3cf828e8bc93af762
Nickson K Rop <nickyrutto@gmail.com>
2023-11-16 01:18:03 -0800

    display maximum tempterature of each state

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- display average temperature by city ordered by temperature
-SELECT state, MAX(value) FROM temperatures ORDER BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
