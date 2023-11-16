d1e53ec07c0efcbe0b7e45170070864c9e8bb634
Nickson K Rop <nickyrutto@gmail.com>
2023-11-16 03:22:59 -0800

    lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_show_genres.genre_id AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
 FROM tv_show_genres
 GROUP BY tv_show_genres.genre_id
-HAVING COUNT(tv_show_genres.tv_show_id) > 0
+HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
