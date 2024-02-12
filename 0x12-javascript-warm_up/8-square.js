db1e881229da7921c988356d6fc1ed6739e61b14
Nickson K Rop <nickyrutto@gmail.com>
2024-02-12 15:29:51 -0800

    semistandard compliant

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const x = process.argv[2];
+if (!parseInt(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    let myVar = '';
+    for (let j = 0; j < parseInt(x); j++) {
+      myVar = myVar + 'X'.repeat(parseInt(x)) + '\n';
+    }
+    console.log(myVar);
+  }
+}
