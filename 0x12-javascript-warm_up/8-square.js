0e32d8954cb66dedcb7c6b09a551ae8f43ff31c4
Nickson K Rop <nickyrutto@gmail.com>
2024-02-12 15:43:53 -0800

    print a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,13 @@ const x = process.argv[2];
 if (!parseInt(x)) {
   console.log('Missing size');
 } else {
-  let myVar = '';
   for (let j = 0; j < parseInt(x); j++) {
-    myVar = myVar + 'X'.repeat(parseInt(x)) + '\n';
+    let y = 0;
+    let myVar = '';
+    while (y < x) {
+      myVar = myVar + 'X';
+      y++;
+    }
+    console.log(myVar);
   }
-  console.log(myVar);
 }
