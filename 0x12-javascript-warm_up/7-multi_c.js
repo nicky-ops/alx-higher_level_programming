093e13e83e5fde6ad5343cc9a883cf8c278da032
Nickson K Rop <nickyrutto@gmail.com>
2024-02-12 15:13:15 -0800

    fix semistandard error

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 if (!parseInt(process.argv[2])) {
   console.log('Missing number of occurrences');
-} else if (parseInt(process.argv[2]) <= 0) {
-  } else {
+} else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('C is fun');
   }
