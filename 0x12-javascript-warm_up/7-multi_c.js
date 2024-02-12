2c9a59ec233aa0c58b97836f5abfb3b6c06245fc
Nickson K Rop <nickyrutto@gmail.com>
2024-02-12 15:01:24 -0800

    fixed an error;

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-if (!parseInt(process.argv[2]) || parseInt(process.argv[2]) <= 0) {
+if (!parseInt(process.argv[2])) {
   console.log('Missing number of occurrences');
-} else {
+} else if (parseInt(process.argv[2]) <= 0) {
+  } else {
   const message = 'C is fun\n';
-  console.log(message.repeat(parseInt(process.argv[2])));
+  console.log(message.repeat(parseInt(process.argv[2]) - 1) + 'C is fun');
 }
