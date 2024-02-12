2b0538900e0df2a7b838039868239b106049c3f0
Nickson K Rop <nickyrutto@gmail.com>
2024-02-12 14:36:33 -0800

    fix an errot

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log(parseInt(process.argv[2]) ? 'C is fun\n'.repeat(parseInt(process.argv[2])) : 'Missing number of occurences');
+console.log(parseInt(process.argv[2]) ? 'C is fun\n'.repeat(parseInt(process.argv[2]) - 1) + 'C is fun' : 'Missing number of occurences');
