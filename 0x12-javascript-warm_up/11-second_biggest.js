7d6e52a5832f46e5b59dcfc0d53be9ece1ed6058
Nickson K Rop <nickyrutto@gmail.com>
2024-02-12 16:25:23 -0800

    second biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const len = process.argv.length;
-if (len <= 3) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const args = process.map(Number).slice(2, len).sort((a, b) => a - b);
-
-  console.log(args[len - 2]);
+  const args = process.argv
+    .map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
