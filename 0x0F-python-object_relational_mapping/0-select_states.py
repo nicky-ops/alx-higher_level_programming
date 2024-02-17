6aadcf4368de4f70ece868d22244899fcc624a42
Nickson K Rop <nickyrutto@gmail.com>
2024-02-17 12:31:00 -0800

    get database name from the command line

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -19,8 +19,7 @@ RETURNS:
 """
 
 if __name__ == "__main__":
-    db_name = "hbtn_0e_0_usa"
-    db = MySQLdb.connect(host="localhost", port=3306,  user=sys.argv[1], passwd=sys.argv[2], db=db_name)
+    db = MySQLdb.connect(host="localhost", port=3306,  user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
 
