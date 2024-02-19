92c3b33e404da5158465a422628c644074435f4a
Nickson K Rop <nickyrutto@gmail.com>
2024-02-19 11:20:29 -0800

    fix pycodestlye error

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     engine = create_engine(url.format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.contains('a')).order_by(State.id)
+    states = session.query(State).filter(State.name.contains('a'))
     for instance in states:
         print("{0}: {1}".format(instance.id, instance.name))
 
