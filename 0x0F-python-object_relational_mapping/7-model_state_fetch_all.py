2c84832b1b7394b0cbd47159fcfb2d747e3b3734
Nickson K Rop <nickyrutto@gmail.com>
2024-02-19 09:08:17 -0800

    fix pycodestlyle eror

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,9 +11,15 @@ if __name__ == "__main__":
     """
     Lists all the states
     """
+<<<<<<< HEAD
     url = 'mysql+mysqldb://{}:{}@localhost:3306/{} \
             '.format(sys.argv[1], sys.argv[2], sys.argv[3])
     engine = create_engine(url)
+=======
+    url = 'mysql+mysqldb://{}@localhost/{}'
+    engine = create_engine(url.format(sys.argv[1], sys.argv[2], sys.argv[3]),
+               pool_pre_ping=True)
+>>>>>>> parent of d193c39... add the third argument
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance in session.query(State).order_by(State.id):
