d193c392f03640527d2cd7ac4f71dd7a33535613
Nickson K Rop <nickyrutto@gmail.com>
2024-02-19 08:59:59 -0800

    add the third argument

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     """
     Lists all the states
     """
-    url = 'mysql+mysqldb://{}@localhost/{}'
+    url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(url.format(sys.argv[1], sys.argv[2], sys.argv[3]),
                pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
