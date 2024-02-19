e22c5ef7014ead26fb51429a6d636ade818f56ff
Nickson K Rop <nickyrutto@gmail.com>
2024-02-19 09:14:18 -0800

    fix pycodestyle error

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,18 +11,8 @@ if __name__ == "__main__":
     """
     Lists all the states
     """
-<<<<<<< HEAD
-<<<<<<< HEAD
-    url = 'mysql+mysqldb://{}:{}@localhost:3306/{} \
-            '.format(sys.argv[1], sys.argv[2], sys.argv[3])
-    engine = create_engine(url)
-=======
-=======
->>>>>>> parent of d193c39... add the third argument
-    url = 'mysql+mysqldb://{}@localhost/{}'
-    engine = create_engine(url.format(sys.argv[1], sys.argv[2], sys.argv[3]),
-               pool_pre_ping=True)
->>>>>>> parent of d193c39... add the third argument
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(url.format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance in session.query(State).order_by(State.id):
