472cf81ab29f90664d2aa01ad808809bc3517512
Nickson K Rop <nickyrutto@gmail.com>
2024-02-19 13:29:28 -0800

    define the city model

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
 """
-This module defines the class City that inherits from the Base class from
-the declarative base function
+This script defines a City class
+to work with MySQLAlchemy ORM.
 """
+from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
 
 
 class City(Base):
+    """City class
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The id of the class
+        name (str): The name of the class
+        state_id (int): The state the city belongs to
     """
-    This class defines a model City that inherits from Base
-    It links to the MySQL table cities
 
-    CLASS ATTRIBUTES:
-    id - represents a column of auto-generated, unique integer
-    name - column of a string to store the state name
-    state_id - represents a column of an integer, can't be null and is a foreign key to states.id
-
-    """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    def __repr__(self):
-        """
-        string representation of an object
-        """
-        return f"<User(id={self.id}, name={self.name})>"
+    name = Column(String(128), nullable=False)
