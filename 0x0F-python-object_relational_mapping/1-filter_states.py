4381ee458f2e4f136f616ca86f4888d519ca3f13
Nickson K Rop <nickyrutto@gmail.com>
2024-02-19 00:20:51 -0800

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     db_cursor = db_connect.cursor()
 
-    db_cursor.execute("SELECT * FROM states WHERE name %STARTSWITH 'N'")
+    db_cursor.execute("SELECT * FROM states WHERE name %STARTSWITH 'N' ORDER BY states.id ASC")
 
     rows_selected = db_cursor.fetchall()
 
