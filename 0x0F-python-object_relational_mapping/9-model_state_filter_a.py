f358171d079762d15af7a458ce30320d932a640f
Nickson K Rop <nickyrutto@gmail.com>
2024-02-19 11:16:43 -0800

    fix pycodestyle error

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     engine = create_engine(url.format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.contains('a')).order_by(State.id):
+    states = session.query(State).filter(State.name.contains('a')).order_by(State.id)
+    for instance in states:
         print("{0}: {1}".format(instance.id, instance.name))
 
     session.close()
