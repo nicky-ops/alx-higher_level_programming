b823ef136ad0e78bf7f4869ae2bfd1c4a2768a52
Nickson K Rop <nickyrutto@gmail.com>
2024-02-19 00:53:49 -0800

    fix a spelling error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
 
     db_cursor = db_connect.cursor()
 
-    db_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
-            ORDER BY states.id ASC".fromat(argv[4]))
+    db_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
+            states.id ASC".format(argv[4]))
 
     rows_selected = db_cursor.fetchall()
 
