e86144aca1106e67b1d023892cdb07f878c213c0
Nickson K Rop <nickyrutto@gmail.com>
2024-02-19 05:34:20 -0800

    fix pycodestyle error

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """
-This module defines the class State that inherits from the Base class from 
+This module defines the class State that inherits from the Base class from
 the declarative base function
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
 class State(Base):
     """
     This class defines a model State that inherits from Base
     It links to the MySQL table states
-    
+
     CLASS ATTRIBUTES:
     id - represents a column of auto-generated, unique integer
     name - column of a string to store the state name
@@ -21,7 +23,6 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
 
     def __repr__(self):
         """
