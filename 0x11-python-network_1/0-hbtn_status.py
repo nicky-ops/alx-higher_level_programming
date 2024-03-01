544d27389cf8231a4bce07ac546ee1582d783917
Nickson K Rop <nickyrutto@gmail.com>
2024-03-01 00:14:54 -0800

    fetches a url using urllib package

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import urllib.request
+"""
+this script fetches https://alx-intranet.hbtn.io/status using urllib module
+"""
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        body = html.decode('utf-8')
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", body)
