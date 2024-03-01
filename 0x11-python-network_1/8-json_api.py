3c536c5bc64fd31a3faa6ce42058f842f83b6468
Nickson K Rop <nickyrutto@gmail.com>
2024-03-01 06:31:00 -0800

    parsing to json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+"""
+script takes in a letter
+sends a POST request to http://0.0.0.0:5000/search_user
+"""
+
+
+if __name__ == "__main__":
+    if len(argv) <= 1:
+        q = ""
+    else:
+        q = {'q': argv[1]}
+    url = 'http://0.0.0.0:5000/search_user'
+    r = requests.post(url, data=q)
+    try:
+        data = r.json()
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
