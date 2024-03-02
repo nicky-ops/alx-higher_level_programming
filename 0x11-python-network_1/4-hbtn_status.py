5aed0736e2a469f95300790d2fe0258eda29172b
Nickson K Rop <nickyrutto@gmail.com>
2024-03-01 23:41:03 -0800

    fix error for a case

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,8 +7,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    r = requests.get(url)
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: ", type(r.text))
-    print("\t- content: ", r.text)
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
