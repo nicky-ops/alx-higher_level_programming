6cf361941b9cce357caef74bd230cefa147bc1db
Nickson K Rop <nickyrutto@gmail.com>
2024-03-27 06:15:57 -0700

    make it semistandard compliant

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-requestHTTP = process.argv[2];
+const requestHTTP = process.argv[2];
 request(requestHTTP, function (error, response, body) {
   console.error('error:', error);
   console.log('code: ', response && response.statusCode);
