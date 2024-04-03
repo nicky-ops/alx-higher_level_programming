810bcaafaa0fca9b626598a84fa93534b8eb2f33
Nickson K Rop <nickyrutto@gmail.com>
2024-04-03 08:13:55 -0700

    toggle header using jquery

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,15 @@
+$(document).ready(function ()
+{
+    $('#toggle_header').click(function ()
+    {   if ($('header').hasClass('red'))
+        {
+            $('header').addClass('green');
+            $('header').removeClass('red');
+        }
+        else
+        {
+            $('header').addClass('red');
+            $('header').removeClass('green');
+        }
+    });
+})
