0afeeccd9e883ab878920e6a80d2d66c114a69f6
Nickson K Rop <nickyrutto@gmail.com>
2024-04-03 08:34:35 -0700

    fetch and list all movies from starwars api using ajax jquery

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,15 @@
+$(document).ready(function ()
+{
+    $.ajax(
+        {
+            url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+            dataType: 'json',
+            success: function(data)
+            {
+                $.each(data.results, function(index, film)
+                {
+                    $('#list_movies').append('<li>' + film.title + '</li>');
+                });
+            }
+        });
+});
