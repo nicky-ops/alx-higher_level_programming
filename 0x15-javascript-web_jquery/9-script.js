5e1b7cb9b41533b480ee0b3210d7dc27a109d630
Nickson K Rop <nickyrutto@gmail.com>
2024-04-03 08:40:02 -0700

    using jquery to fetch value of hello from starwars api

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+    $.ajax(
+        {
+            url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+            dataType: 'json',
+            success: function(data) {
+                $('#hello').text(data.hello);
+            }
+        });
+});
